185bcdf33de3da3c931c899bdecb9bb791f0f8a5
CzApAs <bluzkazielona@o2.pl>
2018-03-05 00:08:24 +0100

    klasa postaci, jeszcze niedokonczona

---
+++ Gra/Gra/Postac.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gra
+{
+    class Postac     // do dokonczenia
+    {
+        private int HP;
+        private int MaxHP;
+    }
+}
